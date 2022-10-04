f0a340354ee92d1f79171c39710f7e179be7e898
DineshZingade <zingadedinesh1991@gmail.com>
2022-10-04 17:49:24 +0530

    Added Pop and peekStack Using Linked List

---
+++ UC3AddedPushStackUsingLinkedList/Node.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.linkedlist.UC3AddedPushStackUsingLinkedList;
+
+public class Node<T> {
+	T data;
+	Node next;
+	T element;
+
+	// create a node
+	Node(T d) {
+		data = d;
+		next = null;
+	}
+}
