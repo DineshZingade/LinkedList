2f6e2d9384842c4c03105d89e91b867a2bef3042
DineshZingade <zingadedinesh1991@gmail.com>
2022-10-04 16:51:53 +0530

    Added pop and PeekStach Using LinkedList

---
+++ UC1/addedPopandPeekStack/LinkedList.java
@@ -0,0 +1,102 @@
+package com.bridgelabz.linkedlist.UC1.addedPopandPeekStack;
+
+public class LinkedList<T> {
+	Node<T> head;
+	Node<T> tail;// head
+	Node<T> element;
+
+	public void printList() {
+		Node<T> temp = head;
+		while (temp != null) {
+			System.out.println(temp.data + " ");
+			temp = temp.next;
+		}
+	}
+
+	public Node<T> peek() {
+		return head;
+	}
+
+	void deleteNode(int position) {
+		if (head == null) // empty
+			return;
+		Node temp = head; // store head
+		if (position == 0) {
+			head = temp.next; // change head
+			Node<T> next = temp.next.next;
+			temp.next = next;
+			return;
+		}
+	}
+
+	public int size() {
+		Node<T> temp = head;
+		int count = 0;
+		while (temp != null) {
+			temp = temp.next;
+			count++;
+		}
+		return count;
+	}
+
+	public void insertAfter(Node<T> previousNode, T data) {
+		if (previousNode == null) {
+			System.out.println("previous Node cant be null");
+			return;
+		}
+		Node<T> newNode = new Node<T>(data);
+		newNode.next = previousNode.next;
+		previousNode.next = newNode;
+	}
+
+	public void pop() {
+		if (head == null) {
+			System.out.println("List is empty");
+		}
+		head = head.next;
+	}
+
+	public void add(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+	}
+
+	public Node search(T data) {
+		Node temp = head;
+		while (temp != null) {
+			if (temp.data == data) {
+				return temp;
+			}
+			temp = temp.next;
+			System.out.println(data);
+		}
+		return null;
+	}
+
+	public void popLast() {
+		Node<T> temp = head;
+		Node<T> prevNode = null;
+		while (temp.next != null) {
+			prevNode = temp;
+			temp = temp.next;
+		}
+		prevNode.next = null;
+	}
+
+	public void push(T data) {
+		Node<T> newNode = new Node<T>(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			newNode.next = head;
+		}
+		head = newNode;
+	}
+}
---
+++ UC1/addedPopandPeekStack/Node.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.linkedlist.UC1.addedPopandPeekStack;
+
+public class Node<T> {
+	T data;
+	Node next;
+	T element;
+
+	// create a node
+	Node(T d) {
+		data = d;
+		next = null;
+	}
+}
