3751469038e7f6edbd3c74c873a054a05e9e4acb
DineshZingade <zingadedinesh1991@gmail.com>
2022-10-04 17:41:24 +0530

    Added Push Stack Using Linked List.

---
+++ UC1/AddedPushStackUsingLinkedList/Queue.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.linkedlist.UC1.AddedPushStackUsingLinkedList;
+
+public class Queue {
+
+}
---
+++ UC1/AddedPushStackUsingLinkedList/Stack.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.linkedlist.UC1.AddedPushStackUsingLinkedList;
+
+public class Stack<K> {
+	public class Stack<K>{
+	    private final LinkedList<K> list;
+
+	    public Stack() {
+	        list = new LinkedList<>();
+	    }
+
+	    public void pushStack(int element) {
+	        list.add(element);
+	    }
+	    public void printStack(){ list.printList();}
+
+	    public static void main(String[] args) {
+	        Stack<Integer> stack = new Stack<>();
+	        System.out.println("----------UC1------------");
+	        stack.pushStack(70);
+	        stack.pushStack(30);
+	        stack.pushStack(56);
+	        stack.printStack();
+
+	    }
+	}
+}
