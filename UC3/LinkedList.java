124fd4c64ccd2e10df1e759ff59dfc92d98e689a
DineshZingade <zingadedinesh1991@gmail.com>
2022-10-04 16:05:25 +0530

    UC3 Created

---
+++ UC3/LinkedList.java
@@ -0,0 +1,76 @@
+package com.bridgelabz.linkedlist.UC3;
+
+public class LinkedList<T> {
+	Node<T> head;
+	Node<T> tail;// head
+
+	public void printList() {
+		Node<T> temp = head;
+		while (temp != null) {
+			System.out.println(temp.data + " ");
+			temp = temp.next;
+		}
+	}
+
+	public void insertAfter(Node<T> previousNode, T data) {
+		if (previousNode == null) {
+			System.out.println("previous Node cant be null");
+			return;
+		}
+		Node<T> newNode = new Node<T>(data);
+		newNode.next = previousNode.next;
+		previousNode.next = newNode;
+	}
+
+	public void pop() {
+		if (head == null) {
+			System.out.println("List is empty");
+		}
+		head = head.next;
+	}
+
+	public void add(T data) {
+		Node<T> newNode = new Node<T>(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+	}
+
+	public Node search(T data) {
+		Node temp = head;
+		while (temp != null) {
+			if (temp.data == data) {
+				return temp;
+			}
+			temp = temp.next;
+			System.out.println(data);
+		}
+		return null;
+	}
+
+	public void popLast() {
+		Node<T> temp = head;
+		Node<T> prevNode = null;
+		while (temp.next != null) {
+			prevNode = temp;
+			temp = temp.next;
+		}
+		prevNode.next = null;
+	}
+
+	public void push(T data) {
+		Node<T> newNode = new Node<T>(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			newNode.next = head;
+			head = newNode;
+		}
+	}
+
+}
---
+++ UC3/Node.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.linkedlist.UC3;
+
+public class Node<T> {
+	T data;
+	Node next;
+
+	// create a node
+	Node(T d) {
+		data = d;
+		next = null;
+	}
+
+}
