bfbb66d9a6c469d1b2949590afb59a00602c708c
DineshZingade <zingadedinesh1991@gmail.com>
2022-10-04 16:16:13 +0530

    Added Delete first Element.

---
+++ UC4/LinkedList.java
@@ -0,0 +1,46 @@
+package com.bridgelabz.linkedlist.UC4;
+
+public class LinkedList {
+	Node head;
+	Node tail;// head
+
+	public void printList() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.data + " ");
+			temp = temp.next;
+		}
+	}
+
+	public void insertAfter(Node previousNode, int data) {
+		if (previousNode == null) {
+			System.out.println("previous Node cant be null");
+			return;
+		}
+		Node newNode = new Node(data);
+		newNode.next = previousNode.next;
+		previousNode.next = newNode;
+	}
+
+	public void add(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+	}
+
+	public void push(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			newNode.next = head;
+			head = newNode;
+		}
+	}
+}
---
+++ UC4/MainClass.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.linkedlist.UC4;
+
+public class MainClass {
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		// System.out.println("----------------UC1----------------");
+		// list.add(56);
+		// list.add(30);
+		// list.add(70);
+		// list.printList();
+
+		System.out.println("----------------UC2----------------");
+		list.push(70);
+		list.push(30);
+		list.push(56);
+		list.printList();
+
+		System.out.println("----------------UC3----------------");
+		list.add(56);
+		list.add(70);
+		list.insertAfter(list.head, 30);
+		list.printList();
+	}
+}
