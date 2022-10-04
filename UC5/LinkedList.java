f8c6a5e42ccb312d9cb4e1cdadb53d77028d0574
DineshZingade <zingadedinesh1991@gmail.com>
2022-10-04 16:28:01 +0530

    Delete first element

---
+++ UC5/LinkedList.java
@@ -0,0 +1,54 @@
+package com.bridgelabz.linkedlist.UC5;
+
+public class LinkedList<T> {
+	Node<T> head;
+	Node<T> tail;// head
+
+	public void printList() {
+		Node<T> temp = head;
+		while (temp != null) {
+			System.out.println(temp.data + " ");
+			temp = temp.next;
+		}
+	}
+
+	public void insertAfter(Node<T> previousNode, T data) {
+		if (previousNode == null) {
+			System.out.println("previous Node cant be null");
+			return;
+		}
+		Node<T> newNode = new Node<T>(data);
+		newNode.next = previousNode.next;
+		previousNode.next = newNode;
+	}
+
+	public void pop() {
+		if (head == null) {
+			System.out.println("List is empty");
+		}
+		head = head.next;
+	}
+
+	public void add(T data) {
+		Node<T> newNode = new Node<T>(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+	}
+
+	public void push(T data) {
+		Node<T> newNode = new Node<T>(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			newNode.next = head;
+			head = newNode;
+		}
+	}
+
+}
