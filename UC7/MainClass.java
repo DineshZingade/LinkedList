73ab395dca458a71c4e0800664253278d95fbc1d
DineshZingade <zingadedinesh1991@gmail.com>
2022-10-04 16:40:06 +0530

    Added Delete Last Element

---
+++ UC7/MainClass.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.linkedlist.UC7;
+
+public class MainClass {
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<>();
+		// System.out.println("----------------UC1----------------");
+		// list.add(56);
+		// list.add(30);
+		// list.add(70);
+		// list.printList();
+
+		System.out.println("----------------UC2----------------");
+		list.push(70);
+		list.push(30);
+		list.push(56);
+		list.printList();
+
+		System.out.println("----------------UC3----------------");
+		list.printList();
+
+		System.out.println("----------------UC5----------------");
+		// list.add(56);
+		// list.add(30);
+		// list.add(70);
+		// list.popLast();
+		// list.printList();
+
+		System.out.println("----------------UC6----------------");
+		list.add(56);
+		list.add(30);
+		list.add(70);
+		list.search(30);
+		// list.printList();
+
+	}
+}
---
+++ UC7/Node.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.linkedlist.UC7;
+
+public class Node<T> {
+	T data;
+	Node next;
+
+	// create a node
+	Node(T d) {
+		data = d;
+		next = null;
+	}
+}
